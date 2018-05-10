fb15d52bc9bbf492cbeb956f9b51c56aa19f5b1e
Biniam <biniam22@gmail.com>
2018-05-09 23:23:27 -0400

    python intro

--- hello_world.py
+++ hello_world.py
@@ -1,20 +1,20 @@
-# print "Hello World!"
+print "Hello World!"
 # '''
-# x = "Hello Python"
-# print x
-# y = 42
-# print y
+x = "Hello Python"
+print x
+y = 42
+print y
 # '''
-# # define a function that says hello to the name provided
-# # this starts a new block
-# def say_hello(name):
-#   #these lines are indented therefore part of the function
-#   if name:
-#    print 'Hello, ' + name + 'from inside the function'
-#   else:
-#    print 'No name'
-# # now we're unindented and have ended the previous block
-# print 'Outside of the function'
+# define a function that says hello to the name provided
+# this starts a new block
+def say_hello(name):
+  #these lines are indented therefore part of the function
+  if name:
+   print 'Hello, ' + name + 'from inside the function'
+  else:
+   print 'No name'
+# now we're unindented and have ended the previous block
+print 'Outside of the function'
 
 # name = "zen":
 # print "My name is " + name
@@ -22,8 +22,8 @@
 # last_name = "Coder"
 # print "My name is {} {}".format(first_name, last_name)
 #
-for count in range(0, 5):
-    print"looping - ", count
+# for count in range(0, 5):
+#     print"looping - ", count
 # age = 15
 # if age >= 18:
 #   print 'Legal age'
